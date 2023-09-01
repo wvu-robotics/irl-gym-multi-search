b15608c1f487d9858b74c5489fa68d383cfec722
matt <mac00072@mix.wvu.edu>
2023-08-31 20:41:51 -0400

    added mcts_search

--- irl_gym_multi_search/test/decision_maker.py
+++ irl_gym_multi_search/test/decision_maker.py
@@ -1,39 +1,51 @@
 
-# import sys
 import numpy as np
 import random
 
-# from search_algorithms.receding_horizon_search import receding_horizon_search
 from search_algorithms.greedy_search_cells import greedy_search_cells
 from search_algorithms.event_horizon_search import event_horizon
 from search_algorithms.simulated_search import SimBFS
 from search_algorithms.ra_ucb_search import RA_UCB
 from search_algorithms.beam_search import beam_search
 from search_algorithms.depth_limited_search import DLS
-from search_algorithms.mcts_search import MCTS
 from search_algorithms.mcts_search_region import MCTS_Region
-from search_algorithms.mcts_region_search import MCTSWithRegions
-from search_algorithms.greedy_region_search import RegionGreedySearch
-from search_algorithms.regionMCTS_search import RegionMCTSSearch
-
+from search_algorithms.mcts_search import MCTS
+# from search_algorithms.receding_horizon_search import receding_horizon_search
 
 def decision_maker_init(distribution, regions, fov_dict):
-    # greedy_region_search = RegionGreedySearch()
-    mcts_region_search = RegionMCTSSearch()
-    return mcts_region_search
-
-
-# Takes in the probability density at each location using the search distribution
+    init = []
+    return init
 
 def search_decision_maker(size_x, size_y, cur_pos, observation, obstacles, last_action, path, distribution, fov_dict, current_fov, cur_orientation, waypoint_reached, waypoint, regions, mcts_region_search):
 
-    # if waypoint is True: (this means that the agent is currently traveling to a waypoint)
-        # skip over search algs and only do relative pos update. The agent's current position and the waypoint position will go into this.
-        # The waypoint position will come from region mcts if the action > 4 (which would be a move to region action). It should find the closest value to the agent within the selected region. This is the waypoint.
-        # mcts should also return waypoint_active (True/False), and waypoint (actual position of the waypoint). 
-
-    ########################################
+    ################################################
     # Search methods
+
+
+
+    ###############################  MCTS Regions
+
+    if path is not None and (len(path) > 1 or (len(path) == 1 and path[0] is None)): # if there is any leftover path from the previous MCTS run
+        path.pop(0) # remove the first action within the path
+        best_path = path # update the path to be the current path
+    else:
+        region_action_modifier = 0.0036 / (size_x * size_y)
+        mcts_region = MCTS_Region(size_x, size_y, cur_pos, distribution, regions, fov_dict, current_fov, cur_orientation, last_action, obstacles,
+                                  num_iterations=165, c_param=0.3, max_rollout_steps=42, region_action_modifier=region_action_modifier)
+        best_path = mcts_region.simulate()
+    print(best_path)
+
+    ###############################  End of MCTS Regions
+
+    ###############################  MCTS
+
+    # mcts_search = MCTS(size_x, size_y, cur_pos, distribution, fov_dict, current_fov, cur_orientation, last_action, obstacles,
+    #                         num_iterations=165, c_param=0.3, max_rollout_steps=42)
+    # best_path = mcts_search.simulate()
+    # print(best_path)
+
+    ###############################  End of MCTS
+
 
     # relative_pos = greedy_search_cells(cur_pos, distribution) # do greedy search for individual cells
 
@@ -60,33 +72,6 @@ def search_decision_maker(size_x, size_y, cur_pos, observation, obstacles, last_
     # Depth limited search
     # next_pos = DLS(size_x, size_y, cur_pos, distribution, limit=4)
     # relative_pos = np.array(next_pos) - np.array(cur_pos)
-
-    # mcts = MCTS(size_x, size_y, cur_pos, distribution, obstacles, num_iterations=90, c_param=0.2, max_rollout_steps=48)
-    # best_action = mcts.simulate() # Run the MCTS to get the optimal action
-    # relative_pos = np.array(cur_pos) - np.array(cur_pos)
-
-    if path is not None and (len(path) > 1 or (len(path) == 1 and path[0] is None)): # if there is any leftover path from the previous MCTS run
-        path.pop(0) # remove the first action within the path
-        best_path = path # update the path to be the current path
-    else:
-        region_action_modifier = 0.0036 / (size_x * size_y)
-        mcts_region = MCTS_Region(size_x, size_y, cur_pos, distribution, regions, fov_dict, current_fov, cur_orientation, last_action, obstacles,
-                                  num_iterations=165, c_param=0.3, max_rollout_steps=42, region_action_modifier=region_action_modifier)
-        best_path = mcts_region.simulate()
-    
-    print(best_path)
-
-    # relative_pos = np.array(cur_pos) - np.array(cur_pos)
-
-    # mcts_region = MCTSWithRegions(regions, size_x, size_y, cur_pos, distribution, obstacles, num_iterations=300, c_param=0.6, max_rollout_steps=10)
-    # best_action = mcts_region.simulate()
-    # print(best_action)
-    # relative_pos = np.array(cur_pos) - np.array(cur_pos)
-
-
-    # best_action = mcts_region_search.select_next_action(cur_pos, distribution, fov_dict, current_fov, regions)
-    # relative_pos = np.array(cur_pos) - np.array(cur_pos)
-
 
     # best_action = greedy_region_search.select_next_action(cur_pos, distribution, fov_dict, current_fov, regions)
 
--- irl_gym_multi_search/test/search_algorithms/mcts_search.py
+++ irl_gym_multi_search/test/search_algorithms/mcts_search.py
@@ -1,4 +1,8 @@
+
 import numpy as np
+from queue import PriorityQueue
+from concurrent.futures import ProcessPoolExecutor
+import os
 
 class MCTSNode:
     def __init__(self, parent, prior):
@@ -17,13 +21,17 @@ class MCTSNode:
             self.children.append(child)
 
 class MCTS:
-    def __init__(self, size_x, size_y, cur_pos, distribution, obstacles, num_iterations, c_param, max_rollout_steps=150):
+    def __init__(self, size_x, size_y, cur_pos, distribution, fov_dict, current_fov, cur_orientation, last_action, obstacles, num_iterations, c_param, max_rollout_steps=150):
         self.size_x = size_x
         self.size_y = size_y
         self.cur_pos = cur_pos
+        self.last_action = last_action
+        self.current_fov = current_fov
+        self.fov_offsets = [(i - len(current_fov) // 2, j - len(current_fov) // 2) for i in range(len(current_fov)) for j in range(len(current_fov)) if current_fov[i, j] == 1]
+        self.orientation = cur_orientation
+        self.orientation_mapping = {'u': 0, 'r': 1, 'd': 2, 'l': 3}
         self.distribution = distribution
         self.distribution /= np.sum(self.distribution)
-        # self.normalize_rewards()
         self.obstacles = obstacles
         self.num_iterations = num_iterations
         self.c_param = c_param
@@ -33,26 +41,17 @@ class MCTS:
             1: np.array([-1, 0]),  # left
             2: np.array([0, 1]),  # down
             3: np.array([1, 0]),  # right
+            4: np.array([0, 0])   # don't move
         }
         self.total_visits = 0
-        self.root = MCTSNode(None, 1.0)  # Create the root node with None as the parent
+        root_priors = self.compute_priors(self.cur_pos, self.current_fov)
+        self.root = MCTSNode(None, 1.0)  # Create the root node
+        self.root.expand(root_priors)  # Expand the root's children
 
-    def normalize_rewards(self):
-        min_reward = np.min(self.distribution)
-        max_reward = np.max(self.distribution)
-        reward_range = max_reward - min_reward
-
-        if reward_range > 0:
-            self.distribution = (self.distribution - min_reward) / reward_range
-        else:
-            # Handle the special case where all rewards are the same
-            self.distribution = np.ones_like(self.distribution) * 0.5
-
-    # def compute_ucb(self, node):
-    #     # Only compute UCB if the node is being visited for the first time or its visit count changes
-    #     if node.visits == 0 or node.visits != node.parent.visits:
-    #         node.ucb = node.value / (1 + node.visits) + self.c_param * node.prior * np.sqrt(self.total_visits) / (1 + node.visits)
-    #     return node.ucb
+    def action_to_pos(self, action, cur_pos):
+        # Convert the resulting array to a tuple of integers
+        next_pos = tuple(cur_pos + self.actions[action])
+        return next_pos, [action]
 
     def compute_ucb(self, node):
         if node.visits == 0 or node.visits != node.parent.visits:
@@ -60,21 +59,28 @@ class MCTS:
             node.ucb = node.value / (1 + node.visits) + self.c_param * node.prior * np.sqrt(node.parent.visits + 1) / (1 + node.visits)
         return node.ucb
 
-    def compute_priors(self, cur_pos):
+    def compute_reward(self, next_pos, fov):
+        reward = 0
+        if 0 <= next_pos[0] < self.size_x and 0 <= next_pos[1] < self.size_y:
+            for offset in self.fov_offsets:
+                relative_pos = next_pos + offset
+                if (0 <= relative_pos[0] < self.size_x and 0 <= relative_pos[1] < self.size_y and self.obstacles[relative_pos[0], relative_pos[1]] != 1):
+                    reward += self.distribution[relative_pos[0], relative_pos[1]] ** 2
+        else:
+            reward = -np.inf
+        return reward
+
+    def compute_priors(self, cur_pos, fov):
         action_rewards = []
-        for action, delta in self.actions.items():
+        
+        for action in self.actions:
+            delta = self.actions[action]
             next_pos = cur_pos + delta
-            if 0 <= next_pos[0] < self.size_x and 0 <= next_pos[1] < self.size_y:
-                if self.obstacles[next_pos[0], next_pos[1]] == 1:
-                    action_rewards.append(-np.inf)
-                else:
-                    # Use a power function to emphasize higher rewards
-                    action_rewards.append(self.distribution[next_pos[0], next_pos[1]] ** 2)
-            else:
-                action_rewards.append(-np.inf)
+            reward = self.compute_reward(next_pos, fov)
+            action_rewards.append(reward)
 
         max_reward = max(action_rewards)
-        action_rewards = [np.exp(r - max_reward) if r != -np.inf else 0 for r in action_rewards]
+        action_rewards = [r if r != -np.inf else 0 for r in action_rewards]
         total = sum(action_rewards)
         action_probabilities = [r / total if total != 0 else 1.0 / len(self.actions) for r in action_rewards]
         return action_probabilities
@@ -89,37 +95,15 @@ class MCTS:
                 best_child = child
         return best_child
 
-    def tree_search(self, node):
-        path = [node]
-        cur_pos = self.cur_pos.copy()
-        while len(path[-1].children) != 0:
-            node = self.select_child(path[-1])
-            cur_pos += self.actions[node.action]
-            if not (0 <= cur_pos[0] < self.size_x and 0 <= cur_pos[1] < self.size_y):
-                break
-            path.append(node)
-        if path[-1].visits == 0:
-            reward = self.rollout(path[-1], cur_pos)
-            return path[-1], reward
-        else:
-            priors = self.compute_priors(cur_pos)
-            path[-1].expand(priors)
-            node = self.select_child(path[-1])
-            reward = self.rollout(node, cur_pos)
-            return node, reward
-
-    def rollout(self, node, cur_pos):
-        num_steps = self.max_rollout_steps
+    def rollout(self, action, cur_pos, fov):
         reward = 0
-        cur_pos = cur_pos.copy()
-        for _ in range(num_steps):
-            action_probabilities = self.compute_priors(cur_pos)  # Using priors in the rollout
-            action = np.random.choice(len(self.actions), p=action_probabilities)
-            next_pos = cur_pos + self.actions[action]
-            if not (0 <= next_pos[0] < self.size_x and 0 <= next_pos[1] < self.size_y) or self.obstacles[next_pos[0], next_pos[1]] == 1:
-                break
+        for step in range(self.max_rollout_steps):
+            next_pos, path_or_action = self.action_to_pos(action, cur_pos)
+            if next_pos[0] < 0 or next_pos[0] >= self.size_x or next_pos[1] < 0 or next_pos[1] >= self.size_y:
+                continue
+            if self.in_fov(next_pos, fov, cur_pos):
+                reward += self.distribution[next_pos[0]][next_pos[1]]
             cur_pos = next_pos
-            reward += self.distribution[cur_pos[0], cur_pos[1]]
         return reward
 
     def get_best_action(self, cur_pos):
@@ -132,14 +116,66 @@ class MCTS:
             node.value += reward
             reward *= 0.99  # Discount factor for multi-step ahead reward
 
+    def in_fov(self, pos, fov, cur_pos):
+        relative_pos = np.array(pos) - np.array(cur_pos)
+        fov_center = np.array(fov.shape) // 2
+        fov_pos = fov_center + relative_pos
+
+        if 0 <= fov_pos[0] < fov.shape[0] and 0 <= fov_pos[1] < fov.shape[1]:
+            in_fov = fov[fov_pos[0], fov_pos[1]] == 1
+            return in_fov
+        return False
+
+    def tree_search(self, node):
+        path = [node]
+        cur_pos = np.array(self.cur_pos).flatten()[:2]
+        fov = self.current_fov
+        rotation_degree = 0
+        while len(path[-1].children) != 0:
+            node = self.select_child(path[-1])
+            next_pos, path_to_region = self.action_to_pos(node.action, cur_pos) # Unpack the return value here
+            cur_pos = next_pos # Update cur_pos with the new position
+            action_to_rotation = {0: 0, 1: 1, 2: 2, 3: 3, 4: 0}
+            if node.action < 5:
+                rotation_degree = action_to_rotation[node.action]
+            fov = np.rot90(fov, k=rotation_degree)
+
+            if not (0 <= cur_pos[0] < self.size_x and 0 <= cur_pos[1] < self.size_y):
+                break
+            path.append(node)
+        if path[-1] is self.root:
+            priors = self.compute_priors(cur_pos, fov)
+            path[-1].expand(priors)
+            node = self.select_child(path[-1])
+            reward = self.rollout(node.action, cur_pos, fov)
+            return node, reward
+        elif path[-1].visits == 0:
+            reward = self.rollout(path[-1].action, cur_pos, fov)
+            return path[-1], reward
+        else:
+            priors = self.compute_priors(cur_pos, fov)
+            path[-1].expand(priors)
+            node = self.select_child(path[-1])
+            reward = self.rollout(node.action, cur_pos, fov)
+            return node, reward
+
     def simulate(self):
+        rewards = []
+
         for _ in range(self.num_iterations):
             node, reward = self.tree_search(self.root)
+            rewards.append(reward)
             path = [node]
             while node is not self.root:
                 node = node.parent
                 path.append(node)
             self.backpropagate(path, reward)
             self.total_visits += 1
-        return self.select_child(self.root).action
 
+        best_action_node = self.select_child(self.root)
+        best_action = best_action_node.action
+        max_reward = max(rewards)
+        if best_action > 4:
+            print('rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr')
+        best_path = [best_action]
+        return best_path
